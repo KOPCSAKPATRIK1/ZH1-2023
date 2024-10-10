16114dc324f7fc20946a43db4798117fedc8bb34
KOPCSAKPATRIK1 <kopcsakpatrik1@gmail.com>
2024-10-10 17:19:42 +0200

    TestCount

---
+++ Tests/TrivialPersonSourceTests.cs
@@ -0,0 +1,19 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class TrivialPersonSourceTests
+    {
+        [Fact]
+        public void TestCount()
+        {
+            var persons = new TrivialPersonSource(12).GetPersons();
+            Assert.Equal(12, persons.Count());
+        }
+    }
+}
