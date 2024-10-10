d61e4e87e093cba369118768b35325236238857c
KOPCSAKPATRIK1 <kopcsakpatrik1@gmail.com>
2024-10-10 17:15:12 +0200

    1. feladat

---
+++ Common/TrivialPersonSource.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class TrivialPersonSource : IPersonSource
+    {
+        public int _n;
+
+        public TrivialPersonSource(int n)
+        {
+            _n = n;
+        }
+
+        public IEnumerable<Person> GetPersons()
+        {
+            List<Person> persons = new List<Person>();
+            for (int i = 0; i < _n; i++)
+            {
+                persons.Add(Person.GetMe());
+            }
+            return persons;
+        }
+    }
+}
