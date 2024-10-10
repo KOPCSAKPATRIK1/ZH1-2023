b3bbb77eb1bb7a1aef32211bb67d127af7321187
KOPCSAKPATRIK1 <kopcsakpatrik1@gmail.com>
2024-10-10 18:02:00 +0200

    6

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Common;
+
+TrivialPersonSource trivialPersonSource = new TrivialPersonSource(3);
+AdvancedPersonSource advancedPersonSource = new AdvancedPersonSource(new List<Person>());
+for (int i = 0; i < 4; i++)
+{
+    advancedPersonSource.Add($"Name{i}", i, i);
+}
+
+foreach (Person person in advancedPersonSource.GetPersons())
+{
+    Console.WriteLine(person.ToString()); 
+}
+
+foreach (Person person in trivialPersonSource.GetPersons())
+{
+    Console.WriteLine(person.ToString());
+}
