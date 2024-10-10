2c64e469553252d0bcc818ecd8295c12e197e314
KOPCSAKPATRIK1 <kopcsakpatrik1@gmail.com>
2024-10-10 17:34:52 +0200

    5. feladat

---
+++ Tests/AdvancedPersonSourceTests.cs
@@ -0,0 +1,21 @@
+﻿using Common;
+
+namespace Tests
+{
+    public class AdvancedPersonSourceTests
+    {
+        AdvancedPersonSource _personSource = new AdvancedPersonSource(new List<Person>());
+
+        [Fact]
+        public void AddThreePersons()
+        {
+            _personSource.Add("Name1", 1, 1);
+            _personSource.Add("Name2", 1, 1);
+            _personSource.Add("Name3", 1, 1);
+
+            var persons = _personSource.GetPersons();
+
+            Assert.Equal(3, persons.Count());
+        }
+    }
+}
